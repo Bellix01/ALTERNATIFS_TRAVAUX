08b8f1f6896adebdceee95a7d10cdea697d4714a
BELHADJ AHMED AMMINE <149061370+Bellix01@users.noreply.github.com>
2025-03-17 16:00:04 +0000

    Update ProtectedRoute.jsx

--- src/supersecret-admin-panel-a/components/ProtectedRoute.jsx
+++ src/supersecret-admin-panel-a/components/ProtectedRoute.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-    const isLoggedIn = !!localStorage.getItem('authToken'); // Replace with your auth logic
-    console.log('dd',localStorage.getItem('authToken'));
+    const isLoggedIn = !!localStorage.getItem('authToken');
     return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
 };
 
